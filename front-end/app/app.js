88778de2a60f3de3ad8500b211002469e7a3aea0
Vladyslav Pyshnenko <pisha91@gmail.com>
2015-06-16 16:51:39 +0300

    Add skeleton for (front-end) part of registration.

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -4,8 +4,15 @@
 angular.module('travellerNetwork', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider
+      .when('/', {
+        templateUrl : 'views/main.html'
+      })
       .when('/login',{
           templateUrl : 'views/login.html',
           controller : 'loginCtrl'
+      })
+      .when('/registration', {
+        templateUrl: 'views/registration.html',
+        controller: 'registrationCtrl'
       });
 }]);
---
+++ front-end/app/controllers/registrationCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('travellerNetwork')
+.controller('', ['$scope', function($scope){
+	
+}]);
