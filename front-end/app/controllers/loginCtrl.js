75068419a0abc39a12ab569558ecaf861468a1f5
Vladyslav Pyshnenko <pisha91@gmail.com>
2015-06-19 17:36:22 +0300

    Small fixes for sign in button on front-end part

--- front-end/app/controllers/loginCtrl.js
+++ front-end/app/controllers/loginCtrl.js
@@ -2,7 +2,6 @@
 
 angular.module('travellerNetwork')
 .controller('loginCtrl', ['$scope', function($scope) {
-        console.log('Hi!');
         $scope.SignIn = function(){
           console.log('sign in ' + $scope.login + ' ' + $scope.password);
         };
