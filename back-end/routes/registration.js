76df60c6eff4baa6addac92f56f90ff4bc14be97
Pisha91 <pisha91@gmail.com>
2015-06-24 09:11:50 +0300

    Add simple method for registration with mongoose

--- back-end/routes/registration.js
+++ back-end/routes/registration.js
@@ -1,7 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/TravellerNetwork');
+var db = mongoose.connection;
+
+db.on('error', function(err){
+	console.error.bind(console, 'connection error: ');
+});
+
+db.on('open', function(callback){
+	console.log('DB connection open');
+});
+
+var Schema = mongoose.Schema;
+
+var accountSchema = new Schema({
+	firstName : String,
+	lastName : String,
+	login: String,
+	email : String,
+	password: String
+});
+
+var Account = mongoose.model('Account', accountSchema);
 
 router.post('/', function(req, res, next){
+	var account = new Account({
+		firstName : req.body.firstName,
+		lastName : req.body.lastName,
+		login: req.body.login,
+		email: req.body.email,
+		password: req.body.password
+	});
+	
+	console.log(req.body);
+	console.log(account);
+	
+	account.save(function(err, account){
+		if(err) {
+			console.log(err);
+		}else{
+			console.log('Account saved.');
+		}
+	});
+	
 	res.send('registration');
 });
 
